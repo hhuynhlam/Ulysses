31a06ad0995d78a24f5b5f3ed160d07fb2d187cb
Hai Huynhlam <hhuynhla@advent.com>
2015-09-17 18:51:26 -0700

    init

---
+++ src/client/core/require-config.js
@@ -0,0 +1,10 @@
+require.config({
+    baseUrl: '',
+    paths: {
+        // Core
+        'sandbox'                       : 'core/sandbox/sandbox',
+
+        // Vendor
+        'bootstrap'                     : 'vendor/bower_components/bootstrap/dist/js/bootstrap.min'
+    }
+});
