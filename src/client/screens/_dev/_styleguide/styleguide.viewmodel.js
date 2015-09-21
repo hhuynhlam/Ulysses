6c8cd647f22a66b73d03e96508014565a482d5b6
Hai Huynhlam <hhuynhla@advent.com>
2015-09-21 07:03:26 -0700

    start kendo styling

--- src/client/screens/_dev/_styleguide/styleguide.viewmodel.js
+++ src/client/screens/_dev/_styleguide/styleguide.viewmodel.js
@@ -1,13 +1,24 @@
 'use strict';
 
-define(function () {
+define(function (require) {
+    var dropdownWidget = require('dropdown.widget');
+    var _mock = require('json!screens/_dev/_styleguide/styleguide.kendo-mock.json');
 
     var StyleGuideViewModel = function () {
         
     };
 
     StyleGuideViewModel.prototype.init = function init () {
-    
+        this.kendoDropDown();
+    };
+
+    StyleGuideViewModel.prototype.kendoDropDown = function kendoDropDown() {
+        dropdownWidget.create({
+            id: 'KendoDropDownList ',
+            dataTextField: 'name',
+            dataValueField: 'id',
+            dataSource: _mock
+        });
     };
 
     return StyleGuideViewModel;
