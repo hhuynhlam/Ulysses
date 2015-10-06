580a52fdb795b0f0d66996bf4c626ae7f9825907
Hai Huynhlam <hhuynhla@advent.com>
2015-10-06 08:20:59 -0700

    jspm init (+1 squashed commit) Squashed commits: [96367b5] jspm init

--- src/client/app.js
+++ src/client/app.js
@@ -1,45 +1,44 @@
 'use strict';
 
-define(function (require) {
-    var ko = require('knockout');
-    var sammy = require('sammy');
-    var sandbox = require('sandbox');
-    require('bootstrap');
+import * as ko from 'knockout';
+import 'bootstrap';
 
-    var AppViewModel = function () {
-        this.isReady = ko.observable(false);
-    };
+// var sammy = require('sammy');
+// var sandbox = require('sandbox');
 
-    // define a new Sammy.Application bound to the #MainView DOM
-    var app = sammy('#MainView');
+var AppViewModel = function () {
+    this.isReady = ko.observable(false);
+};
 
-    // routes
-    require('dev.router')(app);
+// define a new Sammy.Application bound to the #MainView DOM
+// var app = sammy('#MainView');
+
+// routes
+// require('dev.router')(app);
+
+// // 404 Error
+// app.notFound = function () {
+//     window.location.replace('/#/');
+// };
+
+// // override this function so that Sammy doesn't mess with forms
+// app._checkFormSubmission = function() { return false; };
+
+// // Override swap function for post-actions and transitions
+// app.swap = function(content, callback) {
     
-    // 404 Error
-    app.notFound = function () {
-        window.location.replace('/#/');
-    };
+//     // reset all pub/sub
+//     sandbox.msg.reset();
 
-    // override this function so that Sammy doesn't mess with forms
-    app._checkFormSubmission = function() { return false; };
+//     // replace html
+//     app.$element().html(content);
 
-    // Override swap function for post-actions and transitions
-    app.swap = function(content, callback) {
-        
-        // reset all pub/sub
-        sandbox.msg.reset();
+//     // apply callback
+//     if (callback) { callback.apply(); }
+// };
 
-        // replace html
-        app.$element().html(content);
-
-        // apply callback
-        if (callback) { callback.apply(); }
-    };
-
-    // run app
-    $(function() { 
-        ko.applyBindings(new AppViewModel(), document.getElementById('MainView'));
-        app.run(); 
-    });
+// run app
+$(function() { 
+    ko.applyBindings(new AppViewModel(), document.getElementById('MainView'));
+    // app.run(); 
 });
--- src/client/core/jspm_config.js
+++ src/client/core/jspm_config.js
@@ -3,11 +3,47 @@ System.config({
   defaultJSExtensions: true,
   transpiler: "traceur",
   paths: {
-    "github:*": "../../client/vendor/github/*"
+    "github:*"    : "vendor/github/*",
+    "license:*"   : "vendor/license/*",
+    "npm:*"       : "vendor/npm/*"
   },
 
   map: {
-    "traceur": "github:jmcriffey/bower-traceur@0.0.91",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91"
+    "bootstrap"      : "github:twbs/bootstrap@3.3.5",
+    "jquery"         : "github:components/jquery@2.1.4",
+    "k"              : "license:kendo/js",
+    "knockout"       : "github:knockout/knockout@3.3.0",
+    "lodash"         : "npm:lodash@3.10.1",
+    "moment"         : "github:moment/moment@2.10.6",
+    "traceur"        : "github:jmcriffey/bower-traceur@0.0.91",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91",
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert"       : "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process"      : "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util"         : "npm:util@0.10.3"
+    },
+    "github:twbs/bootstrap@3.3.5": {
+      "jquery"       : "github:components/jquery@2.1.4"
+    },
+    "npm:assert@1.3.0": {
+      "util"         : "npm:util@0.10.3"
+    },
+    "npm:inherits@2.0.1": {
+      "util"         : "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:lodash@3.10.1": {
+      "process"      : "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:process@0.11.2": {
+      "assert"       : "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits"     : "npm:inherits@2.0.1",
+      "process"      : "github:jspm/nodelibs-process@0.1.2"
+    }
   }
 });
