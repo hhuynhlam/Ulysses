2b2c4a3a65726e8dcac0e842d4d86248473dc5f9
Hai Huynhlam <hhuynhla@advent.com>
2015-10-08 07:29:26 -0700

    fade and permissions

--- src/client/core/custom-bindings.js
+++ src/client/core/custom-bindings.js
@@ -3,7 +3,27 @@
 import $ from 'jquery';
 import ko from 'knockout';
 
-// Slide down animation
+// Fade animation
+ko.bindingHandlers.fadeVisible = {
+    init: function(element, valueAccessor) {
+        var value = valueAccessor();
+        $(element).toggle(ko.unwrap(value));
+    },
+
+    update: function(element, valueAccessor) {          
+        var value = valueAccessor();
+        
+        if (ko.unwrap(value)) {
+            $(element).fadeIn(500);
+        }
+
+        else {
+            $(element).fadeOut(500);
+        }
+    } 
+};
+
+// Slide animation
 ko.bindingHandlers.slideVisible = {
     init: function(element, valueAccessor) {
         var value = valueAccessor();
