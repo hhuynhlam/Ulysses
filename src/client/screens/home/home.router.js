5672d67621db7de94fbc5457db68a9f6a13b9117
Hai Huynhlam <hhuynhla@advent.com>
2015-10-07 19:37:11 -0700

    smooth scroll & nav

--- src/client/screens/home/home.router.js
+++ src/client/screens/home/home.router.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import $ from 'jquery';
 import ko from 'knockout';
 import sandbox from 'sandbox';
 
@@ -7,14 +8,35 @@ import ViewModel from './home.viewmodel';
 
 var router = function (app) {
 
-    // root
-    app.get('/#/', function (context) {
+    var render = function (context, callback, id) {
         System.import('screens/home/home.html!text').then(function (template) {
             var viewModel = new ViewModel();
             context.swap(sandbox.util.template(template));
             ko.applyBindings(viewModel, document.getElementById('Home'));
             viewModel.init();
+
+            if (callback) { callback(id); }
         });
+    };
+
+    var scrollTo = function (id) {
+        var $anchor = $(id);
+        $(document.body).animate({ 
+            scrollTop: $anchor.offset().top 
+        }, 800);
+    };
+
+    // root
+    app.get('/#/', function (context) {
+        render(context);
+    });
+
+    app.get('/#/:anchor', function (context) {
+        if(!document.getElementById('Home')) {
+            render(context, scrollTo, this.params.anchor);
+        } else {
+            scrollTo(this.params.anchor);
+        }
     });
 
 };
