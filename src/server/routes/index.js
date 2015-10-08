f3b1aa86c91b55b3b07d7dca49be7ff21a77e5cb
Hai Huynhlam <hhuynhla@advent.com>
2015-10-07 21:36:17 -0700

    switch to dirname

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.sendFile(path.resolve(process.env.PWD, 'src/client', './index.html'));
+    res.sendFile(path.resolve(__dirname, '../../client', './index.html'));
 });
 
 module.exports = router;
